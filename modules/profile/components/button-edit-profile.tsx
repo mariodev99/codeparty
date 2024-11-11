b42d7e3c98eaa62e24fc2180433c6b2b54cac1d4
mariodev99 <lucianomariodev@gmail.com>
2024-11-11 20:19:04 +0100

    edit form styles

--- modules/profile/components/button-edit-profile.tsx
+++ modules/profile/components/button-edit-profile.tsx
@@ -93,161 +93,168 @@ export default function ButtonEditProfile({
           <Edit className="stroke-foreground w-6 h-6" />
         </div>
       </SheetTrigger>
+      {/* <SheetTitle>Edit profile</SheetTitle> */}
       <SheetContent
         side={"bottom"}
-        className="h-96 overflow-y-scroll rounded-t-2xl md:rounded-t-3xl"
+        className=" rounded-t-2xl md:rounded-t-3xl p-0  pt-2 border-none"
         onOpenAutoFocus={(e) => e.preventDefault()}
       >
-        <SheetHeader className="flex justify-center">
-          <SheetTitle className="mb-2">Edit profile</SheetTitle>
-        </SheetHeader>
-        <input
-          type="file"
-          onChange={uploadFile}
-          className="hidden"
-          ref={hiddenFileInput}
-        />
-        <div className="flex flex-col items-center">
-          <div className="flex flex-col items-center font-medium">
-            <div
-              className="rounded-full p-2 md:p-3 border-[3px] border-border cursor-pointer"
-              onClick={handleClick}
-            >
-              <div className="relative h-20 md:h-24 w-20 md:w-24 overflow-hidden rounded-full border-2 bg-content-primary flex justify-center items-center">
-                {avatar ? (
-                  <div className="relative w-full h-full flex justify-center items-center group ">
-                    <Image
-                      alt="avatar profile"
-                      src={avatar}
-                      fill
-                      objectFit="cover"
-                      className="group-hover:hidden "
-                    />
-                    <div className="hidden group-hover:block ">
-                      <RotateCcw />
-                    </div>
-                  </div>
-                ) : (
-                  <Upload className="stroke-foreground" />
-                )}
-              </div>
-            </div>
-            <p className="mt-1">Avatar profile</p>
+        <SheetHeader className="flex justify-center border-b border-border">
+          <SheetTitle className="hidden">Edit profile</SheetTitle>
+          <div className=" flex items-center flex-col w-full py-2 text-lg font-medium">
+            <div className="h-2 rounded-full w-14 bg-content-secondary mb-3"></div>
+            <div>Edit profile</div>
           </div>
-          <form
-            action={UpdateProfileWithData}
-            className="flex flex-col gap-5 max-w-[700px]"
-          >
-            <div>
-              <label htmlFor="username" className="text-base font-medium">
-                Username
-              </label>
-              <Input
-                type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <label htmlFor="position" className="text-base font-medium">
-                Position
-              </label>
-              <select
-                name="position"
-                defaultValue={formData.position}
-                className="cursor-pointer appearance-none h-10 rounded-full bg-content-primary border-2 border-border px-3 py-1"
+        </SheetHeader>
+        <div className="overflow-y-scroll h-[450px] py-4">
+          <input
+            type="file"
+            onChange={uploadFile}
+            className="hidden"
+            ref={hiddenFileInput}
+          />
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center font-medium">
+              <div
+                className="rounded-full p-2 md:p-3 border-[3px] border-border cursor-pointer"
+                onClick={handleClick}
               >
-                {positionList.map((item) => (
-                  <option key={item} value={item} className="border">
-                    {item}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="urls" className="text-base font-medium">
-                Urls
-              </label>
-              <Input
-                type="url"
-                name="github"
-                value={formData.github}
-                onChange={handleChange}
-                placeholder="https://github.com/yourprofile"
-              />
-              <Input
-                type="url"
-                name="website"
-                onChange={handleChange}
-                value={formData.website}
-                placeholder="https://portfolio.com"
-                className="mt-2"
-              />
-            </div>
-
-            <div className="flex flex-col ">
-              <label htmlFor="description" className="text-base font-medium">
-                Description
-              </label>
-              <textarea
-                onChange={handleChange}
-                value={formData.description}
-                name="description"
-                className="bg-content-primary rounded-3xl border border-border h-44 p-3"
-                required
-              ></textarea>
-            </div>
-
-            <div>
-              <div className="flex justify-between">
-                <label className="text-base font-medium">Stack</label>
-                <p>{currentStack.length}/4</p>
+                <div className="relative h-20 md:h-24 w-20 md:w-24 overflow-hidden rounded-full border-2 bg-content-primary flex justify-center items-center">
+                  {avatar ? (
+                    <div className="relative w-full h-full flex justify-center items-center group ">
+                      <Image
+                        alt="avatar profile"
+                        src={avatar}
+                        fill
+                        objectFit="cover"
+                        className="group-hover:hidden "
+                      />
+                      <div className="hidden group-hover:block ">
+                        <RotateCcw />
+                      </div>
+                    </div>
+                  ) : (
+                    <Upload className="stroke-foreground" />
+                  )}
+                </div>
               </div>
-              <div className="flex flex-wrap gap-3 py-2 border-b-2 border-border min-h-14">
-                {currentStack.map(({ name, color }) => (
-                  <div key={name}>
+              <p className="mt-1">Avatar profile</p>
+            </div>
+            <form
+              action={UpdateProfileWithData}
+              className="flex flex-col gap-5 max-w-[700px]"
+            >
+              <div>
+                <label htmlFor="username" className="text-base font-medium">
+                  Username
+                </label>
+                <Input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label htmlFor="position" className="text-base font-medium">
+                  Position
+                </label>
+                <select
+                  name="position"
+                  defaultValue={formData.position}
+                  className="cursor-pointer appearance-none h-10 rounded-full bg-content-primary border-2 border-border px-3 py-1"
+                >
+                  {positionList.map((item) => (
+                    <option key={item} value={item} className="border">
+                      {item}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div>
+                <label htmlFor="urls" className="text-base font-medium">
+                  Urls
+                </label>
+                <Input
+                  type="url"
+                  name="github"
+                  value={formData.github}
+                  onChange={handleChange}
+                  placeholder="https://github.com/yourprofile"
+                />
+                <Input
+                  type="url"
+                  name="website"
+                  onChange={handleChange}
+                  value={formData.website}
+                  placeholder="https://portfolio.com"
+                  className="mt-2"
+                />
+              </div>
+
+              <div className="flex flex-col ">
+                <label htmlFor="description" className="text-base font-medium">
+                  Description
+                </label>
+                <textarea
+                  onChange={handleChange}
+                  value={formData.description}
+                  name="description"
+                  className="bg-content-primary rounded-3xl border border-border h-44 p-3"
+                  required
+                ></textarea>
+              </div>
+
+              <div>
+                <div className="flex justify-between">
+                  <label className="text-base font-medium">Stack</label>
+                  <p>{currentStack.length}/4</p>
+                </div>
+                <div className="flex flex-wrap gap-3 py-2 border-b-2 border-border min-h-14">
+                  {currentStack.map(({ name, color }) => (
+                    <div key={name}>
+                      <div
+                        className="rounded-full px-3 py-1 cursor-pointer flex items-center gap-1"
+                        style={{ backgroundColor: `${color}1A`, color: color }}
+                        onClick={() => handleDeleteSkill(name)}
+                      >
+                        {name}
+                        <Cross />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="flex flex-wrap gap-2 ">
+                {skillsList.map((item) => (
+                  <div key={item.name}>
                     <div
-                      className="rounded-full px-3 py-1 cursor-pointer flex items-center gap-1"
-                      style={{ backgroundColor: `${color}1A`, color: color }}
-                      onClick={() => handleDeleteSkill(name)}
+                      className="rounded-full px-3 py-1 cursor-pointer"
+                      style={{
+                        backgroundColor: `${item.color}1A`,
+                        color: item.color,
+                      }}
+                      onClick={() => handleAddSkill(item.color, item.name)}
                     >
-                      {name}
-                      <Cross />
+                      {item.name}
                     </div>
                   </div>
                 ))}
               </div>
-            </div>
 
-            <div className="flex flex-wrap gap-2 ">
-              {skillsList.map((item) => (
-                <div key={item.name}>
-                  <div
-                    className="rounded-full px-3 py-1 cursor-pointer"
-                    style={{
-                      backgroundColor: `${item.color}1A`,
-                      color: item.color,
-                    }}
-                    onClick={() => handleAddSkill(item.color, item.name)}
-                  >
-                    {item.name}
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <SubmitButton
-              pendingText="Updating profile..."
-              type="submit"
-              className="mt-4 font-medium text-black hover:bg-foreground"
-            >
-              Create profile
-            </SubmitButton>
-          </form>
+              <SubmitButton
+                pendingText="Updating profile..."
+                type="submit"
+                className="mt-4 font-medium text-black hover:bg-foreground"
+              >
+                Create profile
+              </SubmitButton>
+            </form>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
