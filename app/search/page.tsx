4215e8b972b9bb7cc2c2d2cd0543480ecd0f8530
mariodev99 <lucianomariodev@gmail.com>
2024-11-20 20:23:06 +0100

    suspense searchContent

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,15 +5,26 @@ import SearchProfiles from "@/modules/profile/components/search-profiles";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
+function SearchContent() {
+  const params = useSearchParams();
+  const query = params.get("q");
+
+  return (
+    <>
+      <SearchForm />
+      {query && <SearchProfiles query={query} />}
+    </>
+  );
+}
+
 export default function Page() {
   const params = useSearchParams();
   const query = params.get("q");
   return (
     <div className="w-full py-4">
-      {/* <Suspense fallback={<div>loading</div>}>
-        <SearchForm />
-        {query && <SearchProfiles query={query} />}
-      </Suspense> */}
+      <Suspense fallback={<div>loading</div>}>
+        <SearchContent />
+      </Suspense>
     </div>
   );
 }
