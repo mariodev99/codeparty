c4d609a80cdfb64e7f488b904892b051b514a804
mariodev99 <lucianomariodev@gmail.com>
2024-11-20 20:12:43 +0100

    wrap SearchForm in suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,13 +3,16 @@
 import SearchForm from "@/components/common/search-form";
 import SearchProfiles from "@/modules/profile/components/search-profiles";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
   const params = useSearchParams();
   const query = params.get("q");
   return (
     <div className="w-full py-4">
-      <SearchForm />
+      <Suspense fallback={<div>loading</div>}>
+        <SearchForm />
+      </Suspense>{" "}
       {query && <SearchProfiles query={query} />}
     </div>
   );
